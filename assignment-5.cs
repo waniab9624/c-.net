b46ce502e47be64cf811f2b017378bd23fd6ec24
waniab9624 <99789126+waniab9624@users.noreply.github.com>
2022-04-25 12:39:03 +0530

    Add files via upload

---
+++ assignment-5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace  ConsoleApplication8{
+public class Program 
+    {
+    public	int rollno=107;
+     public string name= "Abhay";
+    }
+    class  exam: Program
+    {
+  public static void Main()
+        {
+	exam n=new exam();
+Console.WriteLine(n.rollno);
+Console.WriteLine( n.name);
+	Console.ReadLine();
+        }
+    }
+}
